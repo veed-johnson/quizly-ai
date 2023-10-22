d905a59ff796385700e4b9d17f3f8733b816b35a
David Johnson <48daveed@gmail.com>
2023-10-22 09:54:02 +0100

    updated port

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,8 +25,10 @@ app.use("/", router());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-  console.log("Server running on http://localhost:8080/");
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 mongoose.set("strictQuery", false);
